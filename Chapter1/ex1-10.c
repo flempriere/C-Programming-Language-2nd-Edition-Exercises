e579a454215dc7317a8493b70aee42188a462eee
flempriere <flempriere@gmail.com>
2024-05-18 21:01:00 +1000

    corrected Chapter 1 ex10

--- Chapter1/ex1-10.c
+++ Chapter1/ex1-10.c
@@ -3,9 +3,9 @@
 int main() {
     int c;
     while ((c = getchar()) != EOF) {
-        if (c == '\t') printf("\t");
-        else if(c == '\b') printf("\b");
-        else if(c == '\\') printf("\\");
+        if (c == '\t') printf("\\t");
+        else if(c == '\b') printf("\\b");
+        else if(c == '\\') printf("\\\\");
         else (putchar(c));
     }
     return 0;
